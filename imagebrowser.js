803d5f73b5c2d1f462b3637207ec87ea82f0bf82
Georgy Saukov <georgy.saukov@sap.com>
2022-07-16 01:21:20 +0200

    keyinterceptor

--- imagebrowser.js
+++ imagebrowser.js
@@ -1,15 +1,29 @@
 document.body.style.border = "5px solid red";
 
-let path = window.location.pathname;
-
 var elemDiv = document.createElement('div');
 elemDiv.setAttribute("id", "Pagi_N");
 elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
 document.body.appendChild(elemDiv);
 
-document.onkeypress = function(e) {
-    get = window.event?event:e;
-    key = get.keyCode?get.keyCode:get.charCode;
-    key = String.fromCharCode(key);
-    console.log(key);
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    let path = window.location
+
+    if (e.keyCode == '38') {
+        console.log('up arrow ' + path)
+    }
+    else if (e.keyCode == '40') {
+        console.log('down arrow ' + path)
+    }
+    else if (e.keyCode == '37') {
+        console.log('left arrow ' + path)
+    }
+    else if (e.keyCode == '39') {
+        console.log('right arrow ' + path)
+    }
+
 }
