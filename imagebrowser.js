9b17ef78621d6146ed515f2280652830a1763c10
Georgy Saukov <georgy.saukov@sap.com>
2022-07-16 01:08:04 +0200

    keyinterceptor and configs

--- imagebrowser.js
+++ imagebrowser.js
@@ -6,3 +6,10 @@ var elemDiv = document.createElement('div');
 elemDiv.setAttribute("id", "Pagi_N");
 elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
 document.body.appendChild(elemDiv);
+
+document.onkeypress = function(e) {
+    get = window.event?event:e;
+    key = get.keyCode?get.keyCode:get.charCode;
+    key = String.fromCharCode(key);
+    console.log(key);
+}
