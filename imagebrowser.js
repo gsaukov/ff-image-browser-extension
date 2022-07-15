c698df4f11cc5ef7b17ca3a82d07c72e974284b9
Georgy Saukov <georgy.saukov@sap.com>
2022-07-15 20:04:19 +0200

    div placement.

--- imagebrowser.js
+++ imagebrowser.js
@@ -1,1 +1,8 @@
 document.body.style.border = "5px solid red";
+
+let path = window.location.pathname;
+
+var elemDiv = document.createElement('div');
+elemDiv.setAttribute("id", "Pagi_N");
+elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
+document.body.appendChild(elemDiv);
