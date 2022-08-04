d2b160cb5f3d98e8653bff36531a851c35a8e09c
Georgy Saukov <georgy.saukov@sap.com>
2022-08-04 18:07:34 +0200

    imagesaver draft extension

--- imagesaver/imagesaver.js
+++ imagesaver/imagesaver.js
@@ -1,3 +1,5 @@
+//https://stackoverflow.com/questions/42169569/javascript-download-an-on-the-fly-generated-blob-file-into-specific-folder
+
 console.log("imagesaver loaded")
 
 document.onclick = processClick;
@@ -7,7 +9,24 @@ function processClick(e) {
     e = e || window.event;
 
     if(e.target.tagName === "IMG" && e.altKey) {
-        debugger
+        download(e.target.src)
     }
 
+}
+
+async function download(url) {
+    const a = document.createElement("a");
+    a.href = await toDataURL(url);
+    a.download = "myImage.png";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+function toDataURL(url) {
+    return fetch(url).then((response) => {
+        return response.blob();
+    }).then(blob => {
+        return URL.createObjectURL(blob);
+    });
 }
