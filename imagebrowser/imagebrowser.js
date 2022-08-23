32dd1715dc71dd6bb5c628474b44f1daad6e812d
gsaukov <48772739+gsaukov@users.noreply.github.com>
2022-08-24 00:33:00 +0200

    Update imagebrowser.js

--- imagebrowser/imagebrowser.js
+++ imagebrowser/imagebrowser.js
@@ -5,6 +5,8 @@
 // elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
 // document.body.appendChild(elemDiv);
 
+console.log("imagebrowser loaded")
+
 document.onkeydown = checkKey;
 
 function checkKey(e) {
